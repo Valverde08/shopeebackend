012d6043153c4e2b65d20b3c5fd8a1f05331bef9
unknown <valerde88@outlook.com>
2026-03-24 10:12:08 -0300

    pensamento logica para melhorar delete de item

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,30 +1,41 @@
-async function addItem(userCart,item) {
-    userCart.push(item)
+async function addItem(userCart, item) {
+  userCart.push(item);
 }
 
 async function deleteItem(userCart, name) {
-    let index = userCart.findIndex(item => item.name == name)
+  let index = userCart.findIndex((item) => item.name == name);
 
-    if(index != 1) userCart.splice(index,1)
+  let novoCart = userCart[index];
+  // if (index != 1){} userCart.splice(index, 1);
 
-    
+  for (const item in novoCart) {
+    if (novoCart[item] === quantity) novoCart[item]--;
+  }
 }
 
 async function removeItem(userCart, index) {
-    
+  const deleteIndex = index - 1;
+  //é a maior do que - e menor do que o carrinho
+  if ((index) => 0 && index < userCart.length) {
+    userCart.splice(deleteIndex, 1);
+  }
 }
 
 async function calculateTotal(userCart) {
-    const result = userCart.reduce((total, item)=> total + item.subtotal(),0)
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
 
-    console.log(result);
-    
-};
-      
-
-export {
-    addItem,
-    deleteItem,
-    removeItem,
-    calculateTotal
+  console.log(`Valor do carrinho - R$${result}`);
 }
+
+async function displayCart(useCart) {
+  console.log("====================================");
+  console.log(`Shopee Cart`);
+  console.log("====================================");
+  useCart.forEach((element, index) => {
+    console.log(`${index + 1}. ${element.name}- R$${element.price} |
+       Quantity: ${element.quantity} | Subtotal  : ${element.subtotal()}
+        `);
+  });
+}
+
+export { addItem, deleteItem, removeItem, calculateTotal, displayCart };
